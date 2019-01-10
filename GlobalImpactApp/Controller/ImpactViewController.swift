0669dcb48fd090b8905179895b2bb485a7d7a87e
David <davidlarson46@yahoo.com>
2019-01-10 09:05:07 -0700

    fixed some errors, renamed viewcontroller to impactviewcontroller.

--- GlobalImpactApp/Controller/ImpactViewController.swift
+++ GlobalImpactApp/Controller/ImpactViewController.swift
@@ -8,7 +8,7 @@
 import MapKit
 import UIKit
 
-public class ViewController: UIViewController
+public class ImpactViewController: UIViewController
 {
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var animatebutton: UIButton!
@@ -34,7 +34,7 @@ extension MKMapView
 {
     public func animatedZoom(zoomRegion:MKCoordinateRegion, duration:TimeInterval)
     {
-        MKMapView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.6, initialSpringelocity: options: UIView.AnimationOptions.curveEaseIn, animations:
+        MKMapView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 10, options: UIView.AnimationOptions.curveEaseIn, animations:
             {self.setRegion(zoomRegion, animated: true) }, completion: nil)
     }
 }
