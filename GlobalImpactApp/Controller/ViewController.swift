d2a599c641888ecf80716788459559ff68c84f79
David <davidlarson46@yahoo.com>
2019-01-10 09:00:48 -0700

    added some buttons, labels, and images to the view controller, also added some zooming animations on the map part of the view controller.

--- GlobalImpactApp/Controller/ViewController.swift
+++ GlobalImpactApp/Controller/ViewController.swift
@@ -5,16 +5,36 @@
 //  Created by Larson, David on 1/10/19.
 //  Copyright © 2019 CTEC. All rights reserved.
 //
-
+import MapKit
 import UIKit
 
-class ViewController: UIViewController {
+public class ViewController: UIViewController
+{
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var animatebutton: UIButton!
+   @IBOutlet weak var ImpactImage: UIImageView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-
+public override func viewDidLoad()
+{
+    super.viewDidLoad()
 }
 
+@IBAction func animateClick(_ sender: UIButton)
+{
+    let impactLocation = CLLocationCoordinate2D(latitude: -28.6657, longitude: 30.5296)
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 10.05, longitudeDelta: 10.05)
+    let impactRegion = MKCoordinateRegion(center: impactLocation, span: mapSpan)
+    mapView.animatedZoom(zoomRegion: impactRegion, duration: 2.5)
+    
+    }
+}
+
+extension MKMapView
+{
+    public func animatedZoom(zoomRegion:MKCoordinateRegion, duration:TimeInterval)
+    {
+        MKMapView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.6, initialSpringelocity: options: UIView.AnimationOptions.curveEaseIn, animations:
+            {self.setRegion(zoomRegion, animated: true) }, completion: nil)
+    }
+}
